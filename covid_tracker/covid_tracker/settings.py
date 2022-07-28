31a25e6816cd1298f10748baeb300e5b5e4fe312
Sinam <juansantos2kplus2@gmail.com>
2022-07-29 00:26:04 +0800

    with frontend

--- covid_tracker/covid_tracker/settings.py
+++ covid_tracker/covid_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from lib2to3.pytree import Base
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mgnf)nut&6x96)ao*lpav)h4&e$+tm6fa)&tqrt8%ho5nclj=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vaccinated-sorting.herokuapp.com', '127.0.0.1']
 
@@ -120,8 +121,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
