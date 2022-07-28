44e64bfc0c03e43dbfd2c95d1955ad21d67dfe61
Sinam <juansantos2kplus2@gmail.com>
2022-07-28 22:36:42 +0800

    made deployable settings

--- covid_tracker/covid_tracker/settings.py
+++ covid_tracker/covid_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mgnf)nut&6x96)ao*lpav)h4&e$+tm6fa)&tqrt8%ho5nclj=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vaccinated-sorting.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
